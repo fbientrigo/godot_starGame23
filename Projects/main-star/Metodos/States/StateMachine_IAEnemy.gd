688e80c7abcafca4b65d337ef9a1879c4a268ccc
floresenhalloween <fbientrigo@gmail.com>
2023-11-18 19:28:14 -0300

    working november and follow basic

--- Projects/main-star/Metodos/States/StateMachine_IAEnemy.gd
+++ Projects/main-star/Metodos/States/StateMachine_IAEnemy.gd
@@ -11,10 +11,13 @@ var player_area : Area2D
 func _ready():
 	load_states()
 	#on_child_transition(current_state, "EnemyFollow")
-	
 
 
+
+# Activa el seguir al jugador
 func _on_sensor_area_entered(area):
 	player_area = area
 	on_child_transition(current_state, "EnemyFollow")
+
+
 
